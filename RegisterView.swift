591bf1cefa25e6b9abd75f61466f2a51aa8326eb
Auriane Poirier <poirierauriane@gmail.com>
2024-03-13 13:38:45 +0100

    register

---
+++ RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  FestivalDuJeu
+//
+//  Created by Auriane POIRIER on 13/03/2024.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
---
+++ RegisterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewModel.swift
+//  FestivalDuJeu
+//
+//  Created by Auriane POIRIER on 13/03/2024.
+//
+
+import Foundation
