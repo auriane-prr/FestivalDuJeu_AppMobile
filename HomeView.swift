9c9bfc096640add594979ad56721de806ded8fc7
THIRION <114052700+MargauxThirion@users.noreply.github.com>
2024-03-13 13:30:20 +0100

    Update HomeView

--- HomeView.swift
+++ HomeView.swift
@@ -9,34 +9,23 @@ import SwiftUI
 
 struct HomeView: View {
     @EnvironmentObject private var viewModel: AuthViewModel
-    @State private var isAuthenticated: Bool = true
 
     var body: some View {
         NavigationView {
             VStack {
                 Text("Bienvenue dans l'application !")
+                // Autres éléments de votre HomeView
             }
-            .navigationTitle("Accueil")
+            .navigationTitle("Bienvenue \(viewModel.username)")
             .toolbar {
                 ToolbarItemGroup(placement: .navigationBarTrailing) {
                     Button(action: {
-                        viewModel.logout()
+                        viewModel.logout() // S'assurer que cette méthode réinitialise `isAuthenticated` à `false`
                     }) {
                         Text("Logout")
                     }
                 }
             }
-        }
-        .onReceive(viewModel.$isAuthenticated) { isAuthenticated in
-            self.isAuthenticated = isAuthenticated
-            if !isAuthenticated {
-                // Rediriger l'utilisateur vers LoginView
-                viewModel.loginViewPresented = true
-            }
-        }
-        .fullScreenCover(isPresented: $viewModel.loginViewPresented) {
-            LoginView()
-                .environmentObject(viewModel)
         }
     }
 }
