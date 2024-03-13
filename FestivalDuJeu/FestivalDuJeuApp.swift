6aba0b93d0b82a499770b0c8b7e7b5b0c89939f4
Auriane Poirier <poirierauriane@gmail.com>
2024-03-13 08:18:50 +0100

    initial commit

---
+++ FestivalDuJeu/FestivalDuJeuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestivalDuJeuApp.swift
+//  FestivalDuJeu
+//
+//  Created by Auriane POIRIER on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FestivalDuJeuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
