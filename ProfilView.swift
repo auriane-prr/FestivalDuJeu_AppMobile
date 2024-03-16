b6b82b8d45082bfc997617326571ba1094ccacc3
THIRION <114052700+MargauxThirion@users.noreply.github.com>
2024-03-16 18:23:39 +0100

    page profil ok

--- ProfilView.swift
+++ ProfilView.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct ProfilView: View {
-    @StateObject var authViewModel = AuthViewModel()
+    @EnvironmentObject var authModel: AuthViewModel
+    @StateObject var benevoleViewModel = BenevoleViewModel()
 
     var body: some View {
-        NavigationView {
-            Text("Vous êtes sur la page profil")
-            .navigationTitle("Profil")
+        VStack {
+            if let benevole = benevoleViewModel.benevole {
+                // Utilisation de Text pour afficher les informations
+                Text("Bienvenue, \(benevole.pseudo)").font(.headline)
+                Group {
+                    Text("Nom: \(benevole.nom)")
+                    Text("Prénom: \(benevole.prenom)")
+                    Text("Pseudo: \(benevole.pseudo)")
+                    Text("Association: \(benevole.association)")
+                    Text("Taille de T-shirt: \(benevole.taille_tshirt)")
+                    Text("Végétarien: \(benevole.vegetarien ? "Oui" : "Non")")
+                    Text("E-mail: \(benevole.mail)")
+                    Text("Téléphone: \(benevole.num_telephone)")
+                    if let adresse = benevole.adresse {
+                        Text("Adresse: \(adresse)")
+                    }
+                    Text("Hébergement: \(benevole.hebergement)")
+                }
+                .padding(.vertical, 2)
+            } else if benevoleViewModel.isLoading {
+                ProgressView()
+            } else {
+                Text("Impossible de charger les données du bénévole.")
+            }
         }
+        .padding()
+        .onAppear {
+            benevoleViewModel.fetchBenevole(pseudo: authModel.username)
+        }
+        .navigationTitle("Profil")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
